fa13c94cd31bebef2a6fbdf9751c61b6f2a77b02
Brind Sanandiya <brindbsan7@gmail.com>
2024-09-24 09:28:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Help from './Components/Help';
 import { Link, Outlet } from 'react-router-dom';
 import ArrayTable from './Components/ArrayTable';
 import Layout from './Layout';
+import Lab from './Components/US';
 
 function App(props) {
   return (
     <>
       <div className='col-2 d-flex justify-content-end w-100'>
-        <Link className='btn btn-outline-primary flex-fill' to={'/Home'} >Home  </Link>
-        <Link className='btn btn-outline-danger flex-fill' to={'/contact'} >Contact  </Link>
-        <Link className='btn btn-outline-success flex-fill' to={'/about'} >About  </Link>
-        <Link className='btn btn-outline-secondary flex-fill' to={'/help'} >Help</Link>
-        <Link className='btn btn-outline-dark flex-fill' to={'/student'} >Student</Link>
-
+        <Link className='btn btn-outline-primary flex-fill' to={'/Home'} element={<Home/>}> Home  </Link>
+        <Link className='btn btn-outline-danger flex-fill' to={'/contact'} element={<Contact/>}> Contact  </Link>
+        <Link className='btn btn-outline-success flex-fill' to={'/about'} element={<About/>}> About  </Link>
+        <Link className='btn btn-outline-secondary flex-fill' to={'/help'} element={<Help/>}> Help</Link>
+        <Link className='btn btn-outline-dark flex-fill' to={'/student'} element={<ArrayTable/>}> Student</Link>
+        <Link className='btn btn-outline-primary flex-fill' to={'/useState'} element={<Lab/>} > useState </Link>
         {/* <div>{props.name}</div> */}
       </div>
       <Outlet />
